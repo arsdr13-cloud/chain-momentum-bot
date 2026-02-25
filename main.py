9fe0236744396305fbc20232b19416f60af24290
arsdr13-cloud <arsdr13@gmail.com>
2026-02-25 21:14:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -268,6 +268,21 @@ schedule.every(30).minutes.do(smart_engagement)
 
 print("Bot started...")
 
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
+
 while True:
     schedule.run_pending()
     time.sleep(30)
