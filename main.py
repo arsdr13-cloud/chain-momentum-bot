e1c3c5c205ddac54e2dc63ce032e51ccee381e63
arsdr13-cloud <arsdr13@gmail.com>
2026-02-26 11:23:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,10 @@ def health():
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-COINS = ["bitcoin", "ethereum", "solana"]
+print("BOT_TOKEN =", BOT_TOKEN)
+print("CHAT_ID =", CHAT_ID)
 
+COINS = ["bitcoin", "ethereum", "solana"]
 
 # ==============================
 # TELEGRAM SEND FUNCTION
